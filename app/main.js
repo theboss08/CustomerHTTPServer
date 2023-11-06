0ada189a33831b2be2c61bdb1640a0decb1866d9
Arshad Jamal <iarsh@amazon.com>
2023-11-06 22:07:12 +0530

    Respond with content

--- app/main.js
+++ app/main.js
@@ -9,7 +9,7 @@ const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     let firstLine = data.toString().split('\r\n')[0];
     let str = firstLine.split(' ')[1].match(/\/echo\/(.*)/);
-    if(str[1] != '') {
+    if(str && str[1] != '') {
         socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${str[1].length}\r\n${str[1]}\r\n`);
     }
     else {
