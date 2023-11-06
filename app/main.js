157d69bf4c523e221b7a1b1fe9e50d720cbf56e9
Arshad Jamal <iarsh@amazon.com>
2023-11-06 21:35:11 +0530

    Fix

--- app/main.js
+++ app/main.js
@@ -12,7 +12,7 @@ const server = net.createServer((socket) => {
         socket.write('HTTP/1.1 200 OK\r\n\r\n');
     }
     else {
-        socket.write('HTTP/1.1 400 Not Found\r\n\r\n');
+        socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
     }
     socket.end();
   })
