a5440072b3aea16bf29ba8673292d6169a066152
Arshad Jamal <iarsh@amazon.com>
2023-11-06 21:11:37 +0530

    Sending HTTP 200 response

--- app/main.js
+++ app/main.js
@@ -1,14 +1,14 @@
 const net = require("net");
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
-
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
   socket.on("close", () => {
     socket.end();
     server.close();
   });
+  socket.on("data", () => {
+    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+  })
 });
 
 server.listen(4221, "localhost");
