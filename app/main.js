6a72981986156faacbe4539618553910c2528f0c
Arshad Jamal <iarsh@amazon.com>
2023-11-06 22:06:52 +0530

    Respond with content

--- app/main.js
+++ app/main.js
@@ -9,7 +9,12 @@ const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     let firstLine = data.toString().split('\r\n')[0];
     let str = firstLine.split(' ')[1].match(/\/echo\/(.*)/);
-    socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${str[1].length}\r\n${str[1]}\r\n`);
+    if(str[1] != '') {
+        socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${str[1].length}\r\n${str[1]}\r\n`);
+    }
+    else {
+        socket.write('HTTP/1.1 200 OK\r\n\r\n');
+    }
     socket.end();
   })
 });
