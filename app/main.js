698161df79bbb1969b012003bd487eb6fbbc0894
Arshad Jamal <iarsh@amazon.com>
2023-11-06 22:09:56 +0530

    Respond with content

--- app/main.js
+++ app/main.js
@@ -12,9 +12,13 @@ const server = net.createServer((socket) => {
     if(str && str[1] != '') {
         socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${str[1].length}\r\n${str[1]}\r\n`);
     }
-    else {
+    else if (firstLine.split(' ')[1].length === 1 && firstLine.split(' ')[1] === '/') {
         socket.write('HTTP/1.1 200 OK\r\n\r\n');
     }
+    else {
+        socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
+    }
+    // socket.write('HTTP/1.1 200 OK\r\n\r\n');
     socket.end();
   })
 });
