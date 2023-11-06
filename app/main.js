2322bf3a26bbca56c8396fc0addc616b36d96d9e
Arshad Jamal <iarsh@amazon.com>
2023-11-06 23:02:21 +0530

    Fix

--- app/main.js
+++ app/main.js
@@ -34,8 +34,13 @@ const server = net.createServer((socket) => {
         else if (path.match(/\/files\/(.*)/) && path.match(/\/files\/(.*)/)[1] != '') {
             let filePath = directory + path.match(/\/files\/(.*)/)[1];
             console.log(filePath);
-            const file = fs.readFileSync(filePath);
-            socket.write(`HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${file.length}\r\n\r\n${file}`);
+            try {
+                const file = fs.readFileSync(filePath);
+                socket.write(`HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${file.length}\r\n\r\n${file}`);
+            } catch (err) {
+                console.log(err);
+                socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
+            }
         }
         else {
             socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
